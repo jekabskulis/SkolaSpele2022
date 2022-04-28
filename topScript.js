4d6a62a788d3a203042b8c10fdf61c70942e302a
jekabskulis <jekabs.kulis@svg.lv>
2022-04-28 19:27:35 +0100

    Update topScript.js

--- topScript.js
+++ topScript.js
@@ -8,6 +8,11 @@ function getCookie(name)
 
 console.log(cookieTest);
 
+
+
+
+console.log(localStorage.getItem("topPlayerN"));
+
 let playerAge = getCookie("playerA");
 let playerName = getCookie("playerN");
 let playerRegion = getCookie("playerR");
@@ -18,8 +23,39 @@ console.log(playerName);
 console.log(playerRegion);
 console.log(playerPoints);
 
+console.log("LocalStorageTest");
 
+let topOpen = playerNameArr.length;
+
+if(topOpen == 0)
+{
+    let playerNameArr = [];
+    let playerAgeArr = [];
+    let playerRegionArr = [];
+    let playerPointsArr = []; 
+    localStorage.setItem("topPlayerN", playerNameArr);
+    localStorage.setItem("topPlayerA", playerAgeArr);
+    localStorage.setItem("topPlayerR", playerRegionArr);
+    localStorage.setItem("topPlayerP", playerPointsArr);
+}
+
+
+playerNameArr = localStorage.getItem("topPlayerN");
+playerAgeArr = localStorage.getItem("topPlayerA");
+playerRegionArr = localStorage.getItem("topPlayerR");
+playerPointsArr = localStorage.getItem("topPlayerP");
+
+playerNameArr[playerNameArr.length] = playerName;
+playerAgeArr[playerAgeArr.length] = playerAge;
+playerRegionArr[playerRegionArr.length] = playerRegion;
+playerPointsArr[playerNameArr.length] = playerPoints;
 
 localStorage.setItem("topPlayerN", playerNameArr);
 localStorage.setItem("topPlayerA", playerAgeArr);
 localStorage.setItem("topPlayerR", playerRegionArr);
+localStorage.setItem("topPlayerP", playerPointsArr);
+
+console.log(localStorage.getItem("topPlayerN"));
+console.log(localStorage.getItem("topPlayerA"));
+console.log(localStorage.getItem("topPlayerR"));
+console.log(localStorage.getItem("topPlayerP"));
